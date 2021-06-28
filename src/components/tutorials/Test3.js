876aa46fc577c53e695b5d4dd1ef71c8f8425c1d
kuromo <michael.ochsner@hotmail.com>
2021-06-28 18:26:08 +0200

    cli prismjs test

---
+++ src/components/tutorials/Test3.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Test3 = () => (
+  <div>
+    <pre className="command-line"><code className="language-javascript">{ 'sudo apt install xyz' }</code></pre>
+  </div>
+)
+
+export default Test3
