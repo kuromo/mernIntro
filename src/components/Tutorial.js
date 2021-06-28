1a52df890c77c4535cbdf111000ef7d3a8e6154b
kuromo <michael.ochsner@hotmail.com>
2021-06-28 18:27:37 +0200

    same

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -4,18 +4,22 @@ import Test from './tutorials/Test'
 import Test2 from './tutorials/Test2'
 
 class Tutorial extends React.Component {
-	 render() { return (
-	    <div>
-	    	<h1>Tutorials</h1>
-	    	<h3>Part 1:</h3>
-	    	<Test/>
-	    	<h3>Part 2:</h3>
-	    	<Test2/>
-	    </div>
-	)}
-	componentDidMount() {
-	    Prism.highlightAll();
-	}
+  render() {
+    return (
+      <div>
+        <h1>Tutorials</h1>
+        <h3>Part 1:</h3>
+        <Test/>
+        <h3>Part 2:</h3>
+        <Test2/>
+        <h3>Part 3:</h3>
+        <Test3/>
+      </div>
+    )
+  }
+  componentDidMount() {
+    Prism.highlightAll();
+  }
 }
 
 export default Tutorial
